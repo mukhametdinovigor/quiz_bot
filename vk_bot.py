112c75e9f0d057cb706014bf520fa6d7c2d68213
Igor Mukhametdinov <mukhametdinovigor@gmail.com>
2021-11-09 01:58:51 +1100

    Add keyboard to vk echo bot

--- vk_bot.py
+++ vk_bot.py
@@ -9,23 +9,34 @@ env = Env()
 env.read_env()
 
 
-def echo(event, vk_api):
+def create_keyboard():
+    keyboard = VkKeyboard(one_time=True)
+    keyboard.add_button('Новый вопрос', color=VkKeyboardColor.PRIMARY)
+    keyboard.add_button('Сдаться', color=VkKeyboardColor.NEGATIVE)
+    keyboard.add_line()
+    keyboard.add_button('Мой счет', color=VkKeyboardColor.SECONDARY)
+    return keyboard
+
+
+def echo(event, vk_api, keyboard):
 
     vk_api.messages.send(
         user_id=event.user_id,
         message=event.text,
         random_id=random.randint(1, 1000),
+        keyboard=keyboard.get_keyboard(),
     )
 
 
 def main():
     vk_session = vk.VkApi(token=env.str('VK_TOKEN'))
     vk_api = vk_session.get_api()
+    keyboard = create_keyboard()
     longpoll = VkLongPoll(vk_session)
 
     for event in longpoll.listen():
         if event.type == VkEventType.MESSAGE_NEW and event.to_me:
-            echo(event, vk_api)
+            echo(event, vk_api, keyboard)
 
 
 if __name__ == "__main__":
